dd8af163f522e3c5e042eaaaffbd6a17c2293791
Ansh-Malik1 <anshmalik718@gmail.com>
2023-09-20 17:18:41 +0530

    App and index files

---
+++ New folder/src/data.js
@@ -0,0 +1,45 @@
+export const data=[{
+    id:"annually",
+    basic:{
+        price:"$199.99",
+        storage:"500 GB",
+        users: "2",
+        send:"3 GB"
+    },
+    master:{
+        price:"$399.99",
+        storage:"2 TB",
+        users: "10",
+        send:"10 GB"
+    },
+    proffessional:{
+        price:"$249.99",
+        storage:"1 TB",
+        users: "5",
+        send:"10 GB"
+    }
+},
+    {
+    id:"monthly",
+    basic:{
+        price:"$19.99",
+        storage:"500 GB",
+        users: "2",
+        send:"3 GB"
+    },
+    master:{
+        price:"$39.99",
+        storage:"2 TB",
+        users: "10",
+        send:"10 GB"
+    },
+    proffessional:{
+        price:"$24.99",
+        storage:"1 TB",
+        users: "5",
+        send:"10 GB"
+    }
+    }
+
+]
+
